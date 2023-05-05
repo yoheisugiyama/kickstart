e3cce49d0bc3df3e0397e1539d83328fb041437d
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-05-05 14:30:45 +0900

    change deploy environment to sepolia from goerli ve2

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -38,6 +38,7 @@ beforeEach(async()=>{
 
 
 describe('Campaigns', ()=>{
+    
     it('deploys a factory and a campaign', ()=>{
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
@@ -79,6 +80,35 @@ describe('Campaigns', ()=>{
         const request = await campaign.methods.requests(0).call();
 
         assert.equal('Buy batteries', request.description);
+    });
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0], 
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        await campaign.methods
+            .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({from: accounts[0], gas: '1000000' })  
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+       
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+
+        console.log(balance);
+        assert(balance > 104);
+
     });
 
 });
